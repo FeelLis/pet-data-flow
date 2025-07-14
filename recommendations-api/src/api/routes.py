240d701724530b16666d24080c8d081794a6e9b4
Dmitriy <foxyteru@gmail.com>
2025-07-14 22:10:00 +0300

    Add route for many

--- recommendations-api/src/api/routes.py
+++ recommendations-api/src/api/routes.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter
 from geojson_pydantic import Polygon
 from geojson_pydantic.types import PolygonCoords
 from loguru import logger
+from pydantic import BaseModel
 
 from config import DataType, config
 from models.recommendation import Recommendation
@@ -12,25 +13,30 @@ from utils.rabbitmq.producer import producer
 router = APIRouter(prefix="/recommendations")
 
 
-def _find_data_type_by_name(data_type_name: str) -> DataType:
-    for data_type_obj in config.data_types:
-        if data_type_obj.name == data_type_name:
-            return data_type_obj
-    raise ValueError(f"{data_type_name} not recognized as valid data type.")
+class RecommendationInput(BaseModel):
+    description: str
+    data_type: str
+    coordinates: PolygonCoords
 
 
 @router.post(path="/one")
 def upload_one_recommendation(
-    description: str,
-    data_type: str,
-    coordinates: PolygonCoords,
+    recommendation_input: RecommendationInput,
 ):
+    def _find_data_type_by_name(data_type_name: str) -> DataType:
+        for data_type_obj in config.data_types:
+            if data_type_obj.name == data_type_name:
+                return data_type_obj
+        raise ValueError(f"{data_type_name} not recognized as valid data type.")
+
     try:
         recommendation = Recommendation(
             id=str(uuid.uuid4()),
-            description=description,
-            data_type=_find_data_type_by_name(data_type),
-            polygon=Polygon(type="Polygon", coordinates=coordinates),
+            description=recommendation_input.description,
+            data_type=_find_data_type_by_name(recommendation_input.data_type),
+            polygon=Polygon(
+                type="Polygon", coordinates=recommendation_input.coordinates
+            ),
         )
     except ValueError as e:
         logger.exception(e)
@@ -40,3 +46,10 @@ def upload_one_recommendation(
     producer.send_message(recommendation)
 
     return "Successfully sent new recommendation to DB."
+
+
+@router.post(path="/many")
+def upload_many_recommendation(recommendations: list[RecommendationInput]):
+    for recommendation in recommendations:
+        upload_one_recommendation(recommendation)
+    return "Successfully sent new recommendations to DB."
