8a54dbf3bffd542acf99c4949b60aa76338593b4
Dmitriy <foxyteru@gmail.com>
2025-07-15 22:38:57 +0300

    Add deserialization to consumer

--- recommendations-handler/src/utils/rabbitmq/consumer.py
+++ recommendations-handler/src/utils/rabbitmq/consumer.py
@@ -1,49 +1,63 @@
-from typing import Callable
+import json
+from functools import lru_cache
+from typing import Callable, get_type_hints
 
 import aio_pika
 from loguru import logger
 from pika.exceptions import AMQPConnectionError
+from pydantic import BaseModel, ValidationError
 
 
 class RabbitMQConsumer:
     def __init__(self, url: str, queue_name: str, handler: Callable):
         self.url = url
         self.queue_name = queue_name
-        self.connection = None
-        self.channel = None
-        self.queue = None
         self.handler = handler
 
-    async def connect(self):
+    @staticmethod
+    @lru_cache(maxsize=None)
+    def _get_func_parsed_model_type(func: Callable) -> type[BaseModel]:
+        type_hints = get_type_hints(func, include_extras=True)
+        for param_type in type_hints.values():
+            if issubclass(param_type, BaseModel):
+                return param_type
+        raise TypeError(
+            f"Could not find an argument which is a subclass of {BaseModel.__name__} in function {func.__qualname__}."
+        )
+
+    def _deserialize(self, message) -> BaseModel | None:
+        data = json.loads(message.body.decode())
+        model_type = RabbitMQConsumer._get_func_parsed_model_type(self.handler)
+        try:
+            return model_type.model_validate(data)
+        except ValidationError as e:
+            logger.error(
+                f"Failed to deserialize RabbitMQ message to model. "
+                f"Message data: {data}, Model type: {model_type.__name__}, Error: {e}"
+            )
+            return None
+
+    async def _connect(self):
         try:
             logger.info(f"Attempting to connect to RabbitMQ at {self.url}...")
             self.connection = await aio_pika.connect_robust(self.url)
-            self.channel = await self.connection.channel()
-            self.queue = await self.channel.declare_queue(name=self.queue_name)
-            logger.success(
-                f"Successfully connected and declared queue '{self.queue_name}'."
-            )
+            logger.success(f"Connected and declared queue '{self.queue_name}'.")
+
         except AMQPConnectionError as e:
             logger.error(f"Could not connect to RabbitMQ: {e}")
             raise  # TODO: retry mechanism
 
-    async def run(self):
-        while (
-            self.channel
-            and self.connection
-            and not self.connection.is_closed
-            and self.queue
-        ):
-            try:
-                await self.queue.consume(self.handler)
-            except AMQPConnectionError as e:
-                logger.error(f"AMQP connection error during consume start: {e}")
-                break
-            except Exception as e:
-                logger.exception(f"An unexpected error occurred during consuming: {e}")
-        await self.close()
+    async def _consume(self):
+        async with self.connection:
+            channel = await self.connection.channel()
+            queue = await channel.declare_queue(self.queue_name)
+            async with queue.iterator() as queue_iter:
+                async for message in queue_iter:
+                    async with message.process():
+                        data = self._deserialize(message)
+                        if data:
+                            await self.handler(data)  # TODO: handler can be sync.
 
-    async def close(self):
-        if self.connection and not self.connection.is_closed:
-            await self.connection.close()
-            logger.info("RabbitMQ connection closed.")
+    async def run(self):
+        await self._connect()
+        await self._consume()
